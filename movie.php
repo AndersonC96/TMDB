1fc896afeea24a7ac56fbe05ac18423dcc11cd60
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-02-21 18:20:59 -0300

    Update movie.php

--- movie.php
+++ movie.php
@@ -28,8 +28,9 @@
 <p class="m-2 text-center"><b style="color: #20c997">Idiomas</b>: 
   <?php
     foreach($movie_id->spoken_languages as $l){
-      echo '<span>' . $l->english_name . ','. '</span> ';
+      $languages .= $l->english_name . ', ';
     }
+    echo substr($languages, 0, -2);
   ?>
 </p>
 <p class="m-2 text-center"><b style="color: #20c997">Status</b>: 
@@ -45,15 +46,17 @@
 <p class="m-2 text-center"><b style="color: #20c997">Produtoras</b>: 
   <?php
     foreach($movie_id->production_companies as $pc){
-      echo $pc->name.", ";
+      $companies .= $pc->name.", ";
     }
+    echo substr($companies, 0, -2);
   ?>
 </p>
 <p class="m-2 text-center"><b style="color: #20c997">Países de produção</b>:
   <?php
     foreach($movie_id->production_countries as $pco){
-      echo $pco->name. ", ";
+      $country .= $pco->name.", ";
     }
+    echo substr($country, 0, -2);
   ?>
 </p>
 <p class="m-2 text-center"><b style="color: #20c997">Orçamento</b>: 
@@ -84,36 +87,55 @@
   ?>
 </p>
 <br>
+<h3 class="m-2 text-center" style="color: #d63384">Créditos</h3>
+<br>
 <h3 class="m-2 text-center" style="color: #d63384">Trailers</h3>
-<?php
-  foreach($movie_video_id->results as $video){
-    echo '<iframe width="560" height="315" src="'."https://www.youtube.com/embed/".$video->key.'" frameborder="0" allowfullscreen class="rounded"></iframe> ';
-  }
-?>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <?php
+    foreach($movie_video_id->results as $video){
+      echo '<div class="carousel-item active"><iframe width="560" height="315" src="'."https://www.youtube.com/embed/".$video->key.'" frameborder="0" allowfullscreen class="rounded d-block w-100"></iframe></div>';
+    }
+    ?>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Anterior</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Próximo</span>
+  </button>
+</div>
 <br>
 <h3 class="m-2 text-center" style="color: #d63384">Filmes similares</h3>
 <ul>
   <?php
     $count = 4;
     $output="";
+    echo '<div class="row row-cols-1 row-cols-md-3 g-4">';
     foreach($movie_similar_id->results as $sim){
-      $output.='<div class="card text-white bg-dark mb-3" style="width: 18rem;" class="m-2 text-center">
-                  <img src="http://image.tmdb.org/t/p/w300'.$sim->backdrop_path.'" class="card-img-top" alt="Poster">
-                  <div class="card-body">
-                    <h5 class="card-title" style="color: #0dcaf0">'.$sim->title.'</h5>
-                    <p class="card-text">'.$sim->overview.'</p>
-                    <a href="movie.php?id='.$sim->id.'" class="btn btn-primary">Detalhes</a>
-                  </div>
-                  <div class="card-footer">
-                    <small class="text-muted">Data de lançamento: '.date("d/m/Y", strtotime($sim->release_date)).'</b></small>
-                  </div>
-                </div>';
+      $output.='
+      <div class="col">
+        <div class="card h-100">
+          <img src="http://image.tmdb.org/t/p/w300'.$sim->backdrop_path.'" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title" style="color: #0dcaf0">'.$sim->title.'</h5>
+          <p class="card-text text-break" style="color: black">'.$sim->overview.'</p>
+          <a href="movie.php?id='.$sim->id.'" class="btn btn-primary">Detalhes</a>
+          </div>
+          <div class="card-footer">
+          <small class="text-muted">Data de lançamento: '.date("d/m/Y", strtotime($sim->release_date)).'</b></small>
+          </div>
+        </div>
+      </div>';
       if($count <=0){
         break;
         $count--;
       }
     }
     echo $output;
+    echo '</div>';
   ?>
 </ul>
 <?php
