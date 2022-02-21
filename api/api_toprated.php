285156ff14e74884502b3ea66af6a3312937b853
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-02-21 15:51:34 -0300

    Update api_toprated.php

--- api/api_toprated.php
+++ api/api_toprated.php
@@ -1,12 +1,10 @@
 <?php
-
-$ct = curl_init();
-curl_setopt($ct, CURLOPT_URL, "http://api.themoviedb.org/3/movie/top_rated?api_key=" . $apikey . "&language=pt-BR");
-curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ct, CURLOPT_HEADER, FALSE);
-curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
-$response5 = curl_exec($ct);
-curl_close($ct);
-$toprated = json_decode($response5);
-
+    $ct = curl_init();
+    curl_setopt($ct, CURLOPT_URL, "http://api.themoviedb.org/3/movie/top_rated?api_key=" . $apikey . "&language=pt-BR");
+    curl_setopt($ct, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ct, CURLOPT_HEADER, FALSE);
+    curl_setopt($ct, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+    $response5 = curl_exec($ct);
+    curl_close($ct);
+    $toprated = json_decode($response5);
 ?>
