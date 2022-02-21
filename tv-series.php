e7b24a8e904378c123677b6ed700be0a86e6a558
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-02-21 20:25:17 -0300

    Update tv-series.php

--- tv-series.php
+++ tv-series.php
@@ -1,35 +1,58 @@
 <?php
   include "conf/info.php";
-  $title="TV Series";
+  $title="Séries";
   include_once "header.php";
 ?>
-
-    <?php
-      include_once "api/api_tv.php";
-    ?>
-        <h3>On Air TV Show</h3>
-        <hr>
-          <?php
-            foreach($tv_onair->results as $tp){
-              $dd = date('d F Y', strtotime($tp->first_air_date));
-              echo '<h3><a href="tvshow.php?id=' . $tp->id . '">'. $tp->original_name .'</a></h3><p>First Air Date : '.$dd.'</p><p>Popularity : '.round($tp->popularity).'</p>';
-            }
-          ?>
-
-        <h3>Top Rated TV Show</h3>
-        <hr>
-        <ul>
-          <?php
-            foreach($tv_top->results as $tt){
-              $de = date('d F Y', strtotime($tt->first_air_date));
-              echo '<li><a href="tvshow.php?id=' . $tt->id . '"><img src="'.$imgurl_2.''. $tt->poster_path . '"><h4>' . $tt->original_name . "</h4><h5><em>First Air Date : ".$tt->first_air_date."<br />Popularity : " . round($tt->popularity) . "</em></h5></a></li>";
-            }
-          ?>
-        </ul>
-      </div>
-    </div>
-    
-
+<?php
+  include_once "api/api_tv.php";
+?>
+<h3>Séries em exibição</h3>
+<?php
+  echo '<div class="row row-cols-1 row-cols-md-3 g-4">';
+  foreach($tv_onair->results as $tp){// para cada série em exibição
+    echo '<div class="col">
+            <div class="card h-100">
+              <img src="http://image.tmdb.org/t/p/w500'. $tp->backdrop_path . '" class="card-img-top" alt="'.$tp->name.'">
+              <div class="card-body">
+                <h5 class="card-title" style="color: #0dcaf0">'. $tp->original_name . ' | ' . $tp->name .' ('. substr($tp->first_air_date, 0, 4) . ')'.'</h5>
+                <p class="card-text text-break" style="color: black">'.$tp->overview.'</p>
+                <a href="tvshow.php?id='.$tp->id.'" class="btn btn-primary">Detalhes</a>
+              </div>
+              <div class="card-footer">
+                <span class="badge bg-primary">'.$tp->vote_average.'</span>
+                <small class="text-muted">Data de lançamento: '.date("d/m/Y", strtotime($tp->first_air_date)).'</b></small>
+              </div>
+            </div>
+          </div>';
+  }
+  echo '</div>';
+?>
+<br>
+<h3>Séries com as melhores avaliações</h3>
+<ul>
+  <?php
+    echo '<div class="row row-cols-1 row-cols-md-3 g-4">';
+    foreach($tv_top->results as $tt){
+      echo '<div class="col">
+            <div class="card h-100">
+              <img src="http://image.tmdb.org/t/p/w500'. $tt->backdrop_path . '" class="card-img-top" alt="'.$tt->name.'">
+              <div class="card-body">
+                <h5 class="card-title" style="color: #0dcaf0">'. $tt->original_name . ' | ' . $tt->name .' ('. substr($tt->first_air_date, 0, 4) . ')'.'</h5>
+                <p class="card-text text-break" style="color: black">'.$tt->overview.'</p>
+                <a href="tvshow.php?id='.$tt->id.'" class="btn btn-primary">Detalhes</a>
+              </div>
+              <div class="card-footer">
+                <span class="badge bg-primary">'.$tt->vote_average.'</span>
+                <small class="text-muted">Data de lançamento: '.date("d/m/Y", strtotime($tt->first_air_date)).'</b></small>
+              </div>
+            </div>
+          </div>';
+    }
+    echo '</div>';
+  ?>
+</ul>
+</div>
+</div>
 <?php
   include_once "footer.php";
 ?>
