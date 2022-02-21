e425ac549b8b9be2856cbdc4a2139521f16594e0
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-02-21 19:12:03 -0300

    Update now-playing.php

--- now-playing.php
+++ now-playing.php
@@ -10,11 +10,25 @@
   $max = date('d/m/Y', strtotime($nowplaying->dates->maximum));
   echo "<h5>De <b>". $min . "</b>, até <b>" . $max . "</b></h5>";
 ?>
-<hr>
 <?php
+  echo '<div class="row row-cols-1 row-cols-md-3 g-4">';
   foreach($nowplaying->results as $p){
-    echo '<li><a href="movie.php?id=' . $p->id . '"><img src="'.$imgurl_1.''. $p->poster_path . '"><h4>' . $p->original_title . " (" . substr($p->release_date, 0, 4) . ")</h4></a></li>";
+    echo '<div class="col">
+            <div class="card h-100">
+              <img src="http://image.tmdb.org/t/p/w500'. $p->backdrop_path . '" class="card-img-top" alt="'.$p->original_title.'">
+              <div class="card-body">
+                <h5 class="card-title" style="color: #0dcaf0">'. $p->original_title . ' | ' . $p->title .' ('. substr($p->release_date, 0, 4) . ')'.'</h5>
+                <p class="card-text text-break" style="color: black">'.$p->overview.'</p>
+                <a href="movie.php?id='.$p->id.'" class="btn btn-primary">Detalhes</a>
+              </div>
+              <div class="card-footer">
+                <span class="badge bg-primary">'.$p->vote_average.'</span>
+                <small class="text-muted">Data de lançamento: '.date("d/m/Y", strtotime($p->release_date)).'</b></small>
+              </div>
+            </div>
+          </div>';
   }
+  echo '</div>';
 ?>
 <?php
   include_once "footer.php";
