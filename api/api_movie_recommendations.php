243de07ff0fc56203189abe7fa1b8e680bd185a6
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-02-21 22:51:39 -0300

    Create api_movie_recommendations.php

---
+++ api/api_movie_recommendations.php
@@ -0,0 +1,10 @@
+<?php
+    $cz = curl_init();
+    curl_setopt($cz, CURLOPT_URL, "http://api.themoviedb.org/3/movie/".$id_movie."/recommendations?api_key=" . $apikey . "&language=pt-BR");
+    curl_setopt($cz, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($cz, CURLOPT_HEADER, FALSE);
+    curl_setopt($cz, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+    $response99 = curl_exec($cz);
+    curl_close($cz);
+    $movie_recommendations_id = json_decode($response99);
+?>
