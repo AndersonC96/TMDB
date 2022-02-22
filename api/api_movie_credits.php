d0af92c6c74ba80c6cb7298868a9349ed86661eb
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-02-21 22:20:09 -0300

    Update api_movie_credits.php

--- api/api_movie_credits.php
+++ api/api_movie_credits.php
@@ -1,10 +1,10 @@
 <?php
-    $cs = curl_init();
-    curl_setopt($cs, CURLOPT_URL, "http://api.themoviedb.org/3/movie/".$id_movie."/credits?api_key=" . $apikey . "&language=pt-BR");
-    curl_setopt($cs, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($cs, CURLOPT_HEADER, FALSE);
-    curl_setopt($cs, CURLOPT_HTTPHEADER, array("Accept: application/json"));
-    $response99 = curl_exec($cs);
-    curl_close($cs);
+    $cz = curl_init();
+    curl_setopt($cz, CURLOPT_URL, "http://api.themoviedb.org/3/movie/".$id_movie."/credits?api_key=" . $apikey . "&language=pt-BR");
+    curl_setopt($cz, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($cz, CURLOPT_HEADER, FALSE);
+    curl_setopt($cz, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+    $response99 = curl_exec($cz);
+    curl_close($cz);
     $movie_credits_id = json_decode($response99);
 ?>
