cae99b49a49e22d771679c875565d2cf216d60a6
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-03-05 21:38:01 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <h2 class="m-2 text-center"><?php echo $sitename ?></h3>
-    <p class="m-2 text-center">Copyright &copy; 2022 - desenvolvido por <a href="https://github.com/AndersonC96">AndersonC96</a></p>
+    <p class="m-2 text-center">Copyright &copy; 2022 - desenvolvido por <a href="https://github.com/AndersonC96" style="text-decoration: none;">AndersonC96</a></p>
   </body>
 </html>
