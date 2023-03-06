9f2c097ef48ff16d879535488e406f66ee74c37f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-03-05 23:49:35 -0300

    Update header.php

--- header.php
+++ header.php
@@ -31,6 +31,9 @@
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="tv-today.php">Séries (Hoje)</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="tv-seriesPopular.php">Séries populares</a>
+          </li>
         </ul>
       </div>
       <form class="d-flex" action="search.php" method="get">
